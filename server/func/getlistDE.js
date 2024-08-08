aef863908720f6fcbfe9ed17d1988987c6a39ec5
luuphuphat69 <luuphuphat69@gmail.com>
2024-08-08 18:36:42 +0700

    fix getlistDE

--- server/func/getlistDE.js
+++ server/func/getlistDE.js
@@ -2,7 +2,6 @@ const DE = require('../model/DE');
 const getlistDE = async () => {
     try {
         const documentEntities = await DE.find({});
-        await mongoose.disconnect();
         return documentEntities;
     } catch (error) {
         console.error("Error fetching document entities:", error);
